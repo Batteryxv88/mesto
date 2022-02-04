9f34b0ebde7b1bc47baeb71e7b3a89115534e94e
Maxim Kovalev <batteryxv88@yandex.ru>
2022-02-04 09:28:34 +0300

    small_fexes

--- script/index.js
+++ script/index.js
@@ -7,30 +7,37 @@ let nameOutput = document.querySelector('.profile__title');
 let jobOutput = document.querySelector('.profile__subtitle');
 let form = document.querySelector('.form');
 
-function fillEditForm() {
+function fillEditForm() {    // Вставляем данные из формы в профиль
   nameOutput.textContent = nameInput.value
   jobOutput.textContent = jobInput.value
-}
-
-function openPopup() {
-  popup.classList.add('popup_active')
 };
 
-formOpenButton.addEventListener('click', () => {
-  openPopup()
-});
+function fillInputForm() {   // Вставляем данные из профиля в форму
+  nameInput.value = nameOutput.textContent
+  jobInput.value = jobOutput.textContent
+};
 
-function closePopup() {
+function openPopup() {   // Открытие попапа
+  popup.classList.add('popup_active')
+  fillInputForm()
+};
+
+function submitForm(evt) {  // Отправка, закрытие формы
+  evt.preventDefault();
+  fillEditForm();
+  closePopup();
+};
+
+formOpenButton.addEventListener('click', openPopup);
+
+
+function closePopup() {      // Закрытие попапа
   popup.classList.remove('popup_active')
-}
+};
+
 closeButton.addEventListener('click', closePopup);
 
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  fillEditForm()
-  closePopup()
-});
+form.addEventListener('submit', submitForm);
 
 
 
